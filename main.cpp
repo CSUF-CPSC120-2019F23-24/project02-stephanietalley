e738e9d03d839843c5097a1756ce29a32132772c
stephanietalley <stephanietalley@cs104u-4h.acad.fullerton.edu>
2019-12-07 13:10:25 -0800

    Re-created program

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@
 
 int main()
 {
-  double total;
-  //Display tracker
-
+  int days;
+  double hotel, meal, total;
   std::string location;
+
+  const int MAX_LOCATION_WIDTH = 20;
+  const int MONEY_COLUMN_WIDTH = 12;
+  const int NUMERIC_COLUMN_WIDTH = 6;
+
+
+  /*Input information recieved from user*/
   std::cout << "What is the trip location? ";
   getline(std::cin, location);
 
-  char days, hotel, meal;
   std::cout << "How many days will the trip take? ";
   std::cin >> days;
 
@@ -21,13 +26,39 @@ int main()
   std::cout << "What is the total meal cost? $";
   std::cin >> meal;
 
-  //.get(last_initial);
-  //std::cin.ignore();
 
-  // create username out of user's first name and middle/last initials
-  std::string tracker = location+days+hotel+meal;
-  std::cout << "Location       Days        Hotel        Meal        Total \n";
-  std::cout << tracker << std::endl;
+  /*Total of expenses*/
+  total = days * (meal + hotel);
 
-    return 0;
+
+  /*Creates String*/
+  std::stringstream sshotel, ssmeal, sstotal, sstrip;
+
+  sshotel << std::setprecision(2) << std::fixed << '$' << hotel;
+  ssmeal  << std::setprecision(2) << std::fixed << '$' << meal;
+  sstotal << std::setprecision(2) << std::fixed << '$' << total;
+
+
+  /*Displays results to user*/
+  std::cout << std::endl << std::left;
+  std::cout << std::setw(MAX_LOCATION_WIDTH) << "Location";
+  std::cout << std::right;
+  std::cout << std::setw(NUMERIC_COLUMN_WIDTH) << "Days";
+  std::cout << std::setw(MONEY_COLUMN_WIDTH) << "Hotel";
+  std::cout << std::setw(MONEY_COLUMN_WIDTH) << "Meal";
+  std::cout << std::setw(MONEY_COLUMN_WIDTH) << "Total" << std::endl;
+
+
+  sstrip << std::left;
+  sstrip << std::setw(MAX_LOCATION_WIDTH) << location.substr(0, MAX_LOCATION_WIDTH - 1);
+  sstrip << std::right;
+  sstrip << std::setw(NUMERIC_COLUMN_WIDTH) << days;
+  sstrip << std::setw(MONEY_COLUMN_WIDTH) << sshotel.str();
+  sstrip << std::setw(MONEY_COLUMN_WIDTH) << ssmeal.str();
+  sstrip << std::setw(MONEY_COLUMN_WIDTH) << sstotal.str() << std::endl;
+
+
+  std::cout << sstrip.str();
+
+  return 0;
 }
